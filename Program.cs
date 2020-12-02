e7f478f8569d332f9a93c9452e7dc6c50cb824f4
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-12-02 13:16:41 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orientacao_a_Objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
